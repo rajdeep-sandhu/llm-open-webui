066a2036f9afe9c934e82bb56d60746f7b5c7a0c
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-01-16 22:32:10 +0000

    amend to use DATA_DIR as secret key location

--- open-webui.bat
+++ open-webui.bat
@@ -1,10 +1,11 @@
 @echo off
 
 set "DATA_DIR=%USERPROFILE%\open-webui\data"
-set /p WEBUI_SECRET_KEY=<"%USERPROFILE%\open-webui\.webui_secret_key"
+set /p WEBUI_SECRET_KEY=<"%DATA_DIR%\.webui_secret_key"
 
-echo This will run Open WebUI
-echo .webui_secret_key should be in %USERPROFILE%\open-webui\
+echo This will run Open WebUI locally as a uv project.
+echo:
+echo .webui_secret_key should be in %DATA_DIR%
 echo Otherwise, Open WebUI will generate a new key, which may limit access to data.
 
 uv run open-webui serve
